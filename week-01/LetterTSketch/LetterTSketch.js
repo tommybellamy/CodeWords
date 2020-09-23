20a4b6f206dae4726a65069f99c9b47a7d11bf96
tommybellamy <69234341+tommybellamy@users.noreply.github.com>
2020-09-23 17:10:31 +1000

    Add files via upload

---
+++ week-01/LetterTSketch/LetterTSketch.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas (500,500);
+background (0);
+strokeWeight (0);
+}
+
+
+function draw() {
+rect (0,0,500,500);
+fill ("blue");
+rect (25,25,200,62); 
+rect (87,75,75,125); 
+rect (500,500,500,500);
+fill("pureple");
+}
